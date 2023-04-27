f689ec6c9e77140e222ddcea4903fcd8b85e664c
SOGIST1 <lbchigges@gmail.com>
2023-04-27 18:08:49 -0400

    A dummy main file to test building

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+
+int main(int argc, char* argv[]) {
+	printf("Hello!\n");
+	return EXIT_SUCCESS;
+}
+/* vim: set tabstop=4 shiftwidth=4 fileencoding=utf-8 noexpandtab: */
