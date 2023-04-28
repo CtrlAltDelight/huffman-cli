fc2c6f25fb008de65ed385c4d6555c35b693be00
SOGIST1 <lbchigges@gmail.com>
2023-04-28 14:46:52 -0400

    main now compresses argv[1] path

--- src/main/main.c
+++ src/main/main.c
@@ -3,9 +3,11 @@
 #include <stdbool.h>
 #include <string.h>
 #include <assert.h>
+#include "huffman.h"
 
 int main(int argc, char* argv[]) {
-	printf("Hello!\n");
+	compress_file(argv[1]);
 	return EXIT_SUCCESS;
 }
+
 /* vim: set tabstop=4 shiftwidth=4 fileencoding=utf-8 noexpandtab: */
